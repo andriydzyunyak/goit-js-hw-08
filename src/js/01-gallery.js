10ef165b04dff408f10c678de3d38f7b90075fea
Andriy Dzyunyak <andriy.dzyunyak@gmail.com>
2022-01-04 01:25:58 +0200

    Add task 1 - gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const itemMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', itemMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href='${original}'>
+        <img class="gallery__image" src='${preview}' alt='${description}'/>
+      </a>`;
+    })
+    .join('');
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+gallery.on('show.simplelightbox', function (event) {
+  `<img src='${event.target.dataset.source}' width="800" height="600">`;
+});
