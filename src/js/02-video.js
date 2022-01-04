0cf59b83a789103007a7983e6843873ebaa5fa25
Andriy Dzyunyak <andriy.dzyunyak@gmail.com>
2022-01-04 02:55:36 +0200

    add task 2 - videoplayer

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(() => {
+    player.getCurrentTime().then(seconds => {
+      console.log(seconds);
+      localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+    });
+  }, 1000),
+);
+
+const toTimeStart = JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0;
+console.log(toTimeStart);
+player.setCurrentTime(toTimeStart);
